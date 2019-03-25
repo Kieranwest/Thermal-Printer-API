b048ea152b1281e2dd60f9e02bb5adba3d6fa222
Kieran West <kieranwest28@gmail.com>
2019-03-25 21:19:05 +0000

    Added Authorization Header

--- server.js
+++ server.js
@@ -15,15 +15,21 @@ const serial = new serialPort('/dev/serial0', {
 
 app.put('/print', (req, res) => {
     const text = req.body.text;
-    if (text == null) {
-        res.status(404).send("Error: The Key Text is invalid or null");
-    } else if (text.length < 5){
-        res.status(400).send("Error: The Key Text does not meet minimum character requirements (5)");
-    } else {
-		serial.write("\n");
-        serial.write(text);
-        res.send(`Text: ${text}`);
-    }
+	const auth = req.headers.authorization;
+	
+	if(auth == "printer"){
+		if (text == null) {
+			res.status(404).send("Error: The Key Text is invalid or null");
+		} else if (text.length < 5){
+			res.status(400).send("Error: The Key Text does not meet minimum character requirements (5)");
+		} else {
+			serial.write("\n");
+			serial.write(text);
+			res.send(`Text: ${text}`);
+		}		
+	} else {
+		res.status(401).send("Error: Authorization Header Invalid");
+	}
 });
 
 app.listen(port, () => console.log(`App Is Listening on Port ${port}`));
